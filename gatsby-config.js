fab2b1228194006b5f1d542cb5b796993a432efd
Sam Marshall <sjmarshy@gmail.com>
2018-03-03 16:12:34 +0000

    feat: add RSS feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,8 @@
 module.exports = {
     siteMetadata: {
-        title: 'The Blog of Sam Marshall',
+        title: 'sjm is typing...',
+        description: 'just another blog',
+        siteUrl: 'https://blog.sjm.computer',
     },
     plugins: [
         'gatsby-plugin-react-helmet',
@@ -10,5 +12,69 @@ module.exports = {
         },
         'gatsby-transformer-remark',
         'gatsby-plugin-styled-components',
+        {
+            resolve: `gatsby-plugin-feed`,
+            options: {
+                query: `
+                {
+                  site {
+                    siteMetadata {
+                      title
+                      description
+                      siteUrl
+                    }
+                  }
+                }
+              `,
+                feeds: [
+                    {
+                        serialize: ({ query: { site, allMarkdownRemark } }) => {
+                            return allMarkdownRemark.edges.map(edge => {
+                                return Object.assign(
+                                    {},
+                                    edge.node.frontmatter,
+                                    {
+                                        description: edge.node.excerpt,
+                                        url:
+                                            site.siteMetadata.siteUrl +
+                                            edge.node.frontmatter.path,
+                                        guid:
+                                            site.siteMetadata.siteUrl +
+                                            edge.node.frontmatter.path,
+                                        custom_elements: [
+                                            {
+                                                'content:encoded':
+                                                    edge.node.html,
+                                            },
+                                        ],
+                                    }
+                                );
+                            });
+                        },
+                        query: `
+                    {
+                      allMarkdownRemark(
+                        limit: 1000,
+                        sort: { order: DESC, fields: [frontmatter___date] },
+                      ) {
+                        edges {
+                          node {
+                            excerpt
+                            html
+                            frontmatter {
+                              title
+                              date
+                              path
+                            }
+                          }
+                        }
+                      }
+                    }
+                  `,
+                        output: '/rss.xml',
+                    },
+                ],
+            },
+        },
     ],
 };
